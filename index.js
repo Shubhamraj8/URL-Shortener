759db9f4979f05521e0a4761d01ab0355a77b5f9
Shubham Raj <shubhamrajrathaur@gmail.com>
2026-01-16 21:22:30 +0530

    fixed mongo connection & clicks anlytics

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ const {connectToMongoDB} = require('./connect')
 const app = express();
 const urlRoute = require("./routes/url");
 const PORT = 4400;
-connectToMongoDB('mongodb://127.0.0.1:27017/url_shortener')
-.then(() => console.log("MongoDB Connected")
-);
+const URL = require ('./models/url');
+
 // app.get("/", (req, res) => {
 //   res.send("Server is running");
 // });
 
+app.use(express.json());
+
 app.use("/url", urlRoute);
 
+app.get("/:shortId", async(req,res) => {
+    const shortId = req.params.id;
+    const entry = await URL.findOneAndUpdate({
+
+    }, { $push: {
+        visitHistory: {
+            timestamp: Date.now()
+        },
+    }
+})
+    res.redirect(entry.redirectURL);
+})
+
+connectToMongoDB('mongodb://127.0.0.1:27017/url_shortener')
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB Connection Error:", err));
+
+
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
-app.use(express.json);
+
+
+
